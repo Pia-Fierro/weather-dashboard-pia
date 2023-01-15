4f71e5aa4f4143cc8d123b76b56296d321fff9eb
pia fierro <piafierro.3187@gmail.com>
2023-01-15 17:46:25 +1000

    city, lat & lon in local storage. Bugg in results

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,22 @@
 var weatherApiKey = "866183740dfa40d11281c75ed90d5b60"
-// to get weather for 5 days:
-// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
 var cityInput = document.getElementById("city-input");
 var searchBtn = document.getElementById("search-btn");
 var city = JSON.parse(localStorage.getItem('searchedCity'));
-var lat = JSON.parse(localStorage.getItem('lat'));
-var lon = JSON.parse(localStorage.getItem('lon'));
+let lat = JSON.parse(localStorage.getItem('lat'));
+let lon = JSON.parse(localStorage.getItem('lon'));
 
 
 // save user city input in local storage when clicking search button.
 function saveCity () {
     var currentCity = cityInput.value;
     localStorage.setItem("searchedCity",JSON.stringify(currentCity));
-    // console.log (currentCity);
+    console.log (currentCity);
 }
 
-searchBtn.addEventListener("click",saveCity,getCoordinates);
+searchBtn.addEventListener("click",saveCity);
 
 // get city coordinates using user city input
-// to get coordinates by city:
 // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
 
 function getCoordinates() {
@@ -32,7 +29,9 @@ function getCoordinates() {
         console.log(data);
 
         var lat =data[0].lat;
+        console.log(lat)
         var lon =data[0].lon;
+        console.log(lon)
         // save latitude and longitude in local storage
         localStorage.setItem("lat",JSON.stringify(lat));
         localStorage.setItem("lon",JSON.stringify(lon));
@@ -40,17 +39,16 @@ function getCoordinates() {
 }
 getCoordinates();
 
-// to get the current weather for user city input, usign city, lat and log from local storage:
-// https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
-function getCurrentWeather () {
-    let request = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + weatherApiKey
+// Current weather and forecast for 5 days for user city input, usign city, lat and log from local storage:
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+function getWeather () {
+    let request = "https://api.openweathermap.org/data/2.5/forecast?lat="+ lat + "&lon=" + lon+ "&appid=" + weatherApiKey
     fetch(request)   
     .then(function(respose) {
             return respose.json();  
         })
     .then(function(data){
         console.log(data);
-        
     })
 }
-getCurrentWeather();
+getWeather();

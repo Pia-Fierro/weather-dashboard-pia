68443bddb289e890e6a3aa2074abc54937a6450f
pia fierro <piafierro.3187@gmail.com>
2023-01-15 18:39:09 +1000

    var causing issues with api response fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,20 +3,25 @@ var weatherApiKey = "866183740dfa40d11281c75ed90d5b60"
 var cityInput = document.getElementById("city-input");
 var searchBtn = document.getElementById("search-btn");
 var city = JSON.parse(localStorage.getItem('searchedCity'));
-let lat = JSON.parse(localStorage.getItem('lat'));
-let lon = JSON.parse(localStorage.getItem('lon'));
+var lat = JSON.parse(localStorage.getItem('lat'));
+var lon = JSON.parse(localStorage.getItem('lon'));
 
 
 // save user city input in local storage when clicking search button.
 function saveCity () {
-    var currentCity = cityInput.value;
-    localStorage.setItem("searchedCity",JSON.stringify(currentCity));
-    console.log (currentCity);
+    city = cityInput.value;
+    localStorage.setItem("searchedCity",JSON.stringify(city));
+    console.log (city);
 }
 
-searchBtn.addEventListener("click",saveCity);
+function main () {
+    saveCity();
+    getCoordinates();
+}
+searchBtn.addEventListener("click",main);
 
-// get city coordinates using user city input
+
+// getting city coordinates using user city input
 // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
 
 function getCoordinates() {
@@ -28,18 +33,18 @@ function getCoordinates() {
     .then(function(data){
         console.log(data);
 
-        var lat =data[0].lat;
+        lat =data[0].lat;
         console.log(lat)
-        var lon =data[0].lon;
+        lon =data[0].lon;
         console.log(lon)
         // save latitude and longitude in local storage
         localStorage.setItem("lat",JSON.stringify(lat));
         localStorage.setItem("lon",JSON.stringify(lon));
+        getWeather();
     })
 }
-getCoordinates();
 
-// Current weather and forecast for 5 days for user city input, usign city, lat and log from local storage:
+// Getting current weather and forecast usign city and lat & log from local storage:
 // api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 function getWeather () {
     let request = "https://api.openweathermap.org/data/2.5/forecast?lat="+ lat + "&lon=" + lon+ "&appid=" + weatherApiKey
@@ -51,4 +56,3 @@ function getWeather () {
         console.log(data);
     })
 }
-getWeather();
